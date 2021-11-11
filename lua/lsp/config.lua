011fd901e7860a7968a65ab2b479b598616662ba
Josh Holloway <josh.holloway@tanium.com>
2021-11-11 10:54:44 +0000

    Add additional language servers

--- lua/lsp/config.lua
+++ lua/lsp/config.lua
@@ -1,3 +1,3 @@
 return {
-    requested_servers = { "sumneko_lua", "rust_analyzer", "gopls", "terraformls" },
+    requested_servers = { "sumneko_lua", "rust_analyzer", "gopls", "terraformls", "jsonls", "pylsp", "tsserver" },
 }
